114fb56004a3ac951a4908c8a55fd56fa350ae66
Zekiye Doğru <72526615+zekiyedogr@users.noreply.github.com>
2024-06-21 18:17:23 +0300

    Update Products.js

--- Dükkan/pages/Products/Products.js
+++ Dükkan/pages/Products/Products.js
@@ -10,41 +10,81 @@ import SearchBar from '../../components/SearchBar';
 import CategoryCard from '../../components/CategoryCard';
 
 const Products = ({ navigation }) => {
-  const { loading, data, error } = useFetch(Config.API_URL);
-  const { loading: loadingC, data: dataC, error: errorC } = useFetch(`${Config.API_URL}/categories`);
+  const { loading, data, error } = useFetch(Config.API_PRODUCT_URL);
+  const { loading: loadingC, data: dataC, error: errorC } = useFetch(`${Config.API_PRODUCT_URL}/categories`);
   const [list, setList] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [category, setCategory] = useState('');
+  const [unfiltered, setUnfiltered] = useState(true);
   
-
   useEffect(() => {
     if (data) {
       setList(data);
     }
   }, [data]);
 
+  useEffect(() => {
+    if (dataC) {
+      const categoriesWithSelection = dataC.map(category => ({
+        name: category,
+        filter: false
+      }));
+      setCategories(categoriesWithSelection);
+      setCategory('');
+    }
+  }, [dataC]);
+
   const handleSearch = text => {
+    const searcedText = text.toLowerCase();
+  
     const filteredList = data.filter(product => {
-      const searcedText = text.toLowerCase();
       const currentTitle = product.title.toLowerCase();
-
-      return currentTitle.indexOf(searcedText) >= 0;
+      const currentCategory = product.category.toLowerCase();
+      const selectedCategory = category.toLowerCase();  
+      
+      if (selectedCategory) {
+        return currentTitle.indexOf(searcedText) >= 0 && currentCategory === selectedCategory;
+      } else {
+        return currentTitle.indexOf(searcedText) >= 0;
+      }
     });
-
+  
     setList(filteredList);
+    console.log(category);
   };
+  
+  
 
   const handleCategorySelect = text => {
+    const searcedText = text.toLowerCase();  
+    
+    const updatedCategories = categories.map(category => {
+      if (category.name.toLowerCase() === searcedText) {
+        return { ...category, filter: true };
+      }
+      return { ...category, filter: false };
+    });
+  
     const filteredList = data.filter(product => {
-      const searcedText = text.toLowerCase();
       const currentTitle = product.category.toLowerCase();
-
       return currentTitle.indexOf(searcedText) >= 0;
     });
-
+  
+    setCategories(updatedCategories);
     setList(filteredList);
-  };
+    setUnfiltered(false);
+    setCategory(text);
+  };  
 
   const removeFilter = () => {
     setList(data);
+    setUnfiltered(true);
+    const resetCategories = categories.map(category => {
+      return { ...category, filter: false };
+    });
+  
+    setCategories(resetCategories);
+    setCategory('');
   }
 
   const handleProductSelect = id => {
@@ -52,17 +92,13 @@ const Products = ({ navigation }) => {
   };
 
   const renderCategory = ({ item }) => (
-    <CategoryCard name={item} onSelect={() => handleCategorySelect(item)} />
+    <CategoryCard name={item.name} onSelect={() => handleCategorySelect(item.name)} filter={item.filter} />
   );
-
-  const allItem = <CategoryCard onSelect={removeFilter}/>
-  
 
   const renderProduct = ({ item }) => (
     <ProductCard product={item} onSelect={() => handleProductSelect(item.id)} />
   );
-
-
+  
   if (loading) {
     return <Loading />;
   }
@@ -77,9 +113,9 @@ const Products = ({ navigation }) => {
       <SearchBar onSearch={handleSearch} />
       {!loadingC && !error ?
         <View style={{flexDirection: 'row'}}>
-          <CategoryCard onSelect={removeFilter}/>
+          <CategoryCard onSelect={removeFilter} filter={unfiltered}/>
           <FlatList
-            data={dataC}
+            data={categories}
             renderItem={renderCategory}
             keyExtractor={(item, index) => index.toString()}
             horizontal
@@ -88,8 +124,6 @@ const Products = ({ navigation }) => {
         </View>
       : null}
 
-      
-      
       <FlatList data={list} renderItem={renderProduct} />
     </SafeAreaView>
   );
