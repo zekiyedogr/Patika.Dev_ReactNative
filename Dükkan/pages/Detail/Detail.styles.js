6c5825419f1c5649d376afe81dbe3ae786b0a8d2
Zekiye Doğru <72526615+zekiyedogr@users.noreply.github.com>
2024-06-21 18:16:54 +0300

    Update Detail.styles.js

--- Dükkan/pages/Detail/Detail.styles.js
+++ Dükkan/pages/Detail/Detail.styles.js
@@ -5,6 +5,7 @@ const deviceSize = Dimensions.get('window');
 export default StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: 'white',
     },
 
     image: {
@@ -12,6 +13,16 @@ export default StyleSheet.create({
         minHeight: deviceSize.height / 3,
         backgroundColor: 'white',
         resizeMode: 'contain',
+    },
+
+    category: {
+        backgroundColor: '#f1f1f1',
+        paddingHorizontal: 8,
+        paddingVertical: 5,
+        fontWeight: 'bold',
+        borderRadius: 3,
+        alignSelf: 'flex-start',
+        textTransform: 'capitalize',        
     },
 
     body_container: {
@@ -36,5 +47,17 @@ export default StyleSheet.create({
         textAlign: 'right',
         marginVertical: 10,
         fontWeight: 'bold',
-    }
+    },
+
+    rating_container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 10,
+    },
+
+    count: {
+        marginLeft: 10,
+        fontSize: 12,
+        fontWeight: 'bold'
+    },
 })
